63ec9f960d0cbe56be6532b6d61e5328673ffa15
c0dexter <michal.dobrowolski1@gmail.com>
2018-09-24 21:14:55 +0200

    Small refactor

--- app/src/main/java/pl/michaldobrowolski/chronews/ui/ArticleDetailActivity.java
+++ app/src/main/java/pl/michaldobrowolski/chronews/ui/ArticleDetailActivity.java
@@ -18,7 +18,7 @@ public class ArticleDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fav);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
